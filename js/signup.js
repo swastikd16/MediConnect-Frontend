c5f1591559484bacb732e145028e31c36d38233e
Priyanshu Kumar Pandey <priyanshupandey44449@gmail.com>
2024-10-26 15:58:27 +0530

    Update

---
+++ js/signup.js
@@ -0,0 +1,40 @@
+document.getElementById('submitbtn').addEventListener('click', async function (event) {
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    // Data to be sent to the backend
+    const data = {
+        request_type: 'new_auth',
+        email: email,
+        password: password,
+    };
+
+    try {
+        // Sending POST request to backend
+        const response = await fetch('http://127.0.0.1:8000/api/auth', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        });
+
+        const result = await response.json();
+        console.log(result);  // Handle response from backend
+
+        if (response.ok) {
+            if (result.status == true) {
+                alert('Sign-up successful!');
+            }
+            else {
+                alert(`Sign-up failed: ${result.message}`);
+            }
+        } else {
+            alert(`Sign-up failed: ${result.message}`);
+        }
+
+    } catch (error) {
+        console.error('Error:', error);
+        alert('An error occurred while submitting the form.');
+    }
+});

24c2f6ff1a8674e52455c5c54be7e52246c1bb65
Shreyash Kumar Dheemar <techshreyash123@gmail.com>
2024-10-26 23:08:15 +0530

    Delete js/SignUp.js

--- js/SignUp.js
+++
@@ -1,6 +0,0 @@
-
-    document.getElementById('user-type').addEventListener('change', function() {
-        const ownerInput = document.getElementById('owner-input');
-        ownerInput.style.display = this.checked ? 'block' : 'none';
-    });
-

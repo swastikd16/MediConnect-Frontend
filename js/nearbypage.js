d28daa67dcd57b36766c13db5d998097b01df4be
TechShreyash <techshreyash123@gmail.com>
2024-10-27 10:57:37 +0530

    Fixes

--- js/nearbypage.js
+++ js/nearbypage.js
@@ -1,4 +1,23 @@
-
+function getLocation() {
+    return new Promise((resolve, reject) => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    const lat = position.coords.latitude;
+                    const long = position.coords.longitude;
+                    resolve({ lat, long });
+                },
+                (error) => {
+                    alert("Error obtaining location");
+                    reject(error);
+                }
+            );
+        } else {
+            alert("Geolocation is not supported by this browser.");
+            reject(new Error("Geolocation is not supported by this browser."));
+        }
+    });
+}
 
 async function getShops() {
     const searchParams = new URLSearchParams(window.location.search);
